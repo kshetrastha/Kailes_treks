62211c4bc77740846c09dc865ba267f43b11e5a6
KshStha <kshetra@dreamnet.my>
2026-02-11 21:34:59 +0545

    .

---
+++ src/Infrastructure/Migrations/20260211154743_WhyWithUsImagePath.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelCleanArch.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class WhyWithUsImagePath : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
