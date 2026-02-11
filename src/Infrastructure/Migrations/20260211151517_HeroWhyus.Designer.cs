30e36083e02bb5303661659263b65a85b5f6081e
KshStha <kshetra@dreamnet.my>
2026-02-11 21:00:49 +0545

    .

--- src/Infrastructure/Migrations/20260211151517_HeroWhyus.Designer.cs
+++ src/Infrastructure/Migrations/20260211151517_HeroWhyus.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TravelCleanArch.Infrastructure.Persistence;
@@ -11,9 +12,11 @@ using TravelCleanArch.Infrastructure.Persistence;
 namespace TravelCleanArch.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260211151517_HeroWhyus")]
+    partial class HeroWhyus
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
