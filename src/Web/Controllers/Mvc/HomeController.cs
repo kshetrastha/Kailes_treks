d259a89b2fd7268c98e4ecc40425d41ca0df713a
kshetrastha <42167216+kshetrastha@users.noreply.github.com>
2026-02-11 16:35:17 +0545

    Align default startup route with root URL

--- src/Web/Controllers/Mvc/HomeController.cs
+++ src/Web/Controllers/Mvc/HomeController.cs
@@ -4,7 +4,6 @@ namespace TravelCleanArch.Web.Controllers.Mvc;
 
 public sealed class HomeController : Controller
 {
-    [HttpGet("/home")]
     public IActionResult Index()
     {
         return View();
