2395b3c5bd1dfb92e1b37936875ac1551a986c9a
KshStha <kshetra@dreamnet.my>
2026-03-13 17:03:49 +0545

    .

--- src/Domain/Entities/Master/ServiceRegionFaq.cs
+++ src/Domain/Entities/Master/ServiceRegionFaq.cs
@@ -1,6 +1,5 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using TravelCleanArch.Domain.Entities;
 
 namespace TravelCleanArch.Domain.Entities.Master;
 
