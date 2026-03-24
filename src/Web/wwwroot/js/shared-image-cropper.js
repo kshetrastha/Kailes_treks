24ecec7ed20f601bdb0ba9e5eac6d02befbe7a4b
kshetrastha <42167216+kshetrastha@users.noreply.github.com>
2026-03-24 17:21:22 +0545

    Fix banner cropper modal sizing and cleanup duplicate crop logic

--- src/Web/wwwroot/js/shared-image-cropper.js
+++ src/Web/wwwroot/js/shared-image-cropper.js
@@ -16,13 +16,20 @@ $(function () {
 
         const style = `
             <style id="${MODAL_STYLE_ID}">
+                #sharedImageCropperModal .modal-dialog {
+                    width: min(96vw, 1280px);
+                    max-width: min(96vw, 1280px);
+                    margin: 2vh auto;
+                }
+
                 #sharedImageCropperModal .modal-content {
-                    min-height: 100vh;
+                    height: 96vh;
                 }
 
                 #sharedImageCropperModal .modal-body {
                     display: flex;
                     min-height: 0;
+                    padding: 0.75rem;
                 }
 
                 #sharedImageCropperModal .shared-image-cropper-stage {
@@ -30,17 +37,19 @@ $(function () {
                     align-items: center;
                     justify-content: center;
                     width: 100%;
-                    min-height: 75vh;
                     height: 100%;
                     overflow: hidden;
                     background: #eef1f7;
                 }
 
-                //#sharedImageCropperModal .shared-image-cropper-stage img {
-                //    display: block;
-                //    max-width: none;
-                //    max-height: none;
-                //}            
+                #sharedImageCropperModal .shared-image-cropper-stage img {
+                    display: block;
+                    width: 100%;
+                    height: 100%;
+                    max-width: none;
+                    max-height: none;
+                    object-fit: contain;
+                }
             </style>
         `;
 
@@ -54,7 +63,7 @@ $(function () {
 
         const modalHtml = `
 <div class="modal fade" id="sharedImageCropperModal" tabindex="-1" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-xl modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title">Crop image</h5>
