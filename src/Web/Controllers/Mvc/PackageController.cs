37acbb74e21cd82db4c9cccc7a94162f791cc442
KshStha <kshetra@dreamnet.my>
2026-04-10 11:36:23 +0545

    Detailses pagechanges

---
+++ src/Web/Controllers/Mvc/PackageController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TravelCleanArch.Application.Abstractions.Persistence;
+
+namespace TravelCleanArch.Web.Controllers.Mvc
+{
+    public class PackageController(IUnitOfWork uow) : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("packages/{slug}")]
+        public async Task<IActionResult> TrekkingDetails(string slug, CancellationToken ct)
+        {
+            var trekkingPacakges = await uow.TrekkingService.GetPublicBySlugAsync(slug, ct);
+            if (trekkingPacakges is null)
+                return NotFound();
+            return View(trekkingPacakges);
+
+
+        }
+    }
+}
